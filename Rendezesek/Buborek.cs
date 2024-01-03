febc6e3313efe45157f729a4eb926cccc651f598
borossandor27 <boros.sanyi@gmail.com>
2024-01-03 09:45:57 +0100

    Add project files.

---
+++ Rendezesek/Buborek.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rendezesek
+{
+    internal class Buborek
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
